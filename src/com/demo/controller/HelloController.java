436802ff98f7cb01b79bca471cf97cd3294c8ead
slcun <slcun@live.com>
2017-08-11 23:36:16 +0800

    ReauestMapping

--- src/com/demo/controller/HelloController.java
+++ src/com/demo/controller/HelloController.java
@@ -13,12 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 2017年8月7日
  */
 @Controller
+@RequestMapping("/user")	//@RequestMapping注解用在类上时,方法上的就会映射为相对于类级别的请求
 public class HelloController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/hello") //对应类上的注解就是这个请求.../user/hello
 	public String hello(Model model){
 		model.addAttribute("message", "Hello SpringMVC");
 		return "hello";
 	}
 
+	
+	
+	
+	
+	
 }
